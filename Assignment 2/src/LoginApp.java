b5717b32f0a538922fda4b164a9f70b7ebd2a7a1
Emaan Roshan <imann.roshan458@gmail.com>
2024-11-29 15:03:13 +0500

    Test2

--- Assignment 2/src/LoginApp.java
+++ Assignment 2/src/LoginApp.java
@@ -45,7 +45,7 @@ public class LoginApp extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
             String email = emailField.getText();
-          
+            String password = new String(passwordField.getPassword());
 
             String userName = authenticateUser(email, password);
             if (userName != null) {
@@ -59,10 +59,10 @@ public class LoginApp extends JFrame {
     String authenticateUser(String email, String password) {
         String userName = null;
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-            String query = "SELECT name FROM User WHERE Email = ?";
+            String query = "SELECT name FROM User WHERE Email = ? AND Password = ?";
             PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, email);
-         
+            stmt.setString(2, password);
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
